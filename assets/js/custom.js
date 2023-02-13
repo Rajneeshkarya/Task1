ed7d1bd070925a0b10ada59b009b45ffd8d19e9c
Rajneeshkarya <rajnirajni54565456@gmail.com>
2023-02-13 20:27:16 +0530

    check first

---
+++ assets/js/custom.js
@@ -0,0 +1,166 @@
+(function ($) {
+	
+	"use strict";
+
+	$(function() {
+        $("#tabs").tabs();
+    });
+
+	$(window).scroll(function() {
+	  var scroll = $(window).scrollTop();
+	  var box = $('.header-text').height();
+	  var header = $('header').height();
+
+	  if (scroll >= box - header) {
+	    $("header").addClass("background-header");
+	  } else {
+	    $("header").addClass("background-header");
+	  }
+	});
+	
+
+	$('.schedule-filter li').on('click', function() {
+        var tsfilter = $(this).data('tsfilter');
+        $('.schedule-filter li').removeClass('active');
+        $(this).addClass('active');
+        if (tsfilter == 'all') {
+            $('.schedule-table').removeClass('filtering');
+            $('.ts-item').removeClass('show');
+        } else {
+            $('.schedule-table').addClass('filtering');
+        }
+        $('.ts-item').each(function() {
+            $(this).removeClass('show');
+            if ($(this).data('tsmeta') == tsfilter) {
+                $(this).addClass('show');
+            }
+        });
+    });
+
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+	
+
+	// Menu Dropdown Toggle
+	if($('.menu-trigger').length){
+		$(".menu-trigger").on('click', function() {	
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	$(document).ready(function () {
+	    $(document).on("scroll", onScroll);
+	    
+	    //smoothscroll
+	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+	        e.preventDefault();
+	        $(document).off("scroll");
+	        
+	        $('a').each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
+	      
+	        var target = this.hash,
+	        menu = target;
+	       	var target = $(this.hash);
+	        $('html, body').stop().animate({
+	            scrollTop: (target.offset().top) + 1
+	        }, 500, 'swing', function () {
+	            window.location.hash = target;
+	            $(document).on("scroll", onScroll);
+	        });
+	    });
+	});
+
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop();
+	    $('.nav a').each(function () {
+	        var currLink = $(this);
+	        var refElement = $(currLink.attr("href"));
+	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $('.nav ul li a').removeClass("active");
+	            currLink.addClass("active");
+	        }
+	        else{
+	            currLink.removeClass("active");
+	        }
+	    });
+	}
+
+
+	// Page loading animation
+	 $(window).on('load', function() {
+
+        $('#js-preloader').addClass('loaded');
+
+    });
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function() {
+		mobileNav();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		var width = $(window).width();
+		$('.submenu').on('click', function() {
+			if(width < 767) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+})(window.jQuery);
+
+
+// Extra Coding  
+ 
+$('#call-to-action').waypoint(function() { 
+		$('.text_1').textillate({
+		  loop: true,
+		  minDisplayTime: 3000, //Loop Interval
+		  initialDelay: 1000, //Waiting time before starting
+		  //when start animation
+		  in: {
+			effect: 'fadeIn', //EffectType(options;http://textillate.js.org/)
+		  }
+		}); 
+ });
+ 
+ // object-fit polyfill run
+// object-fit polyfill run 
+ 
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Scroll in Mobile View
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
+
+if($(window).width()<500){
+	console.log("Inside");
+	$(".gapping").css("height","100px");
+}
+
+$('.li_class_a').on('click', function () {
+    $(this).trigger('dblclick');
+});
